286b545d899d595de118f2d9601f801a7cce1c08
WindOdd <kdlmapcomtw@yahoo.com.tw>
2026-01-06 10:18:31 +0800

    Initial Commit

---
+++ ProjectAkkaApp/ProjectAkkaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectAkkaAppApp.swift
+//  ProjectAkkaApp
+//
+//  Created by Sam Lai on 2026/1/6.
+//
+
+import SwiftUI
+
+@main
+struct ProjectAkkaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
